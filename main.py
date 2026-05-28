51de6a34016085c2d9033edd903d72e33e59a589
Balrahul Sunnoju <rahulsunnoju6@gmail.com>
2026-05-28 10:36:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,38 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
 import pandas
+import datetime as dt
 import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+email = os.environ.get("MY_EMAIL")
+password = os.environ.get("MY_PASSWORD")
+
+today = dt.datetime.now()
+today_day = today.day
+today_month = today.month
+
+for index, row in data.iterrows():
+
+    if today_day == row["day"] and today_month == row["month"]:
+
+        letter_num = random.randint(1, 3)
+
+        with open(f"letter_templates/letter_{letter_num}.txt") as file:
+            f1 = file.read()
+
+        final_letter = f1.replace("[NAME]", row["name"])
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=email, password=password)
+
+            connection.sendmail(
+                from_addr=email,
+                to_addrs=row["email"],
+                msg=f"Subject:Birthday Wishes\n\n{final_letter}"
+            )
